80ed7cf303ef5b57713b0333beca0af5298196cc
WarWolfOne <matheussaito95k@gmail.com>
2024-01-17 11:40:37 -0300

    Organização de diretorios

--- prototipos/Conversor/Conversor.py
+++ prototipos/Conversor/Conversor.py
@@ -4,6 +4,6 @@ file = "data.pdf"
 
 pdfData = tabula.read_pdf(file,pages="all")
 
-output = tabula.convert_into(file, "conversao.csv", output_format="csv", pages=[1,2])
+output = tabula.convert_into(file, "resultados.csv", output_format="csv", pages=[1,2])
 
 print(output)
