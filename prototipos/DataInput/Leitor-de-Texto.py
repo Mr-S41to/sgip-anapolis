e9a394ef52f811cf5e81d562ca5def9d3f458773
WarWolfOne <matheussaito95k@gmail.com>
2024-01-18 14:34:10 -0300

    Extração de dados, identificador dpor inscrição

--- prototipos/DataInput/Leitor-de-Texto.py
+++ prototipos/DataInput/Leitor-de-Texto.py
@@ -1,17 +1,38 @@
 from PyPDF2 import PdfReader
-import tabula
+import re
 
-#Abrir pdf em binários.
-with open("sample.pdf", 'rb') as pdf_file:
-    pdf_reader = PdfReader(pdf_file)
+# Abrir o arquivo PDF em binário.
+with open("sample.pdf", 'rb') as file_pdf:
+    reader_pdf = PdfReader(file_pdf)
 
-    #Obtendo numero de paginas do PFF.
-    num_pages = len(pdf_reader.pages)
+    # Obtendo o número de páginas do PDF.
+    num_paginas = len(reader_pdf.pages)
 
-    #Iterar as páginas do PDF.
-    for page_num in range(num_pages):
-        page = pdf_reader.pages[page_num]
+    # Define um padrão para extrair as informações de endereço.
+    padrao_inscricao = re.compile(r'Matrícula:\s*([\d.]+).*?Inscrição:')
 
-        #Extração de texto da página.
-        text = page.extract_text()
+    # Os endereços extraídos serão armazenados nesta lista.
+    inscricoes = []
+
+    # Iterar pelas páginas do PDF.
+    for num_pagina in range(num_paginas):
+        pagina = reader_pdf.pages[num_pagina]
+
+        # Extração do texto da página.
+        text = pagina.extract_text()
+
+        # Encontrar todas as correspondências para o padrão de endereço em cada página.
+        correspondencias_inscricao = padrao_inscricao.findall(text, re.DOTALL)
+
+        # Processar cada correspondência.
+        for correspondencia in correspondencias_inscricao:
+            # Remover espaços em branco no início e no final.
+            inscricao = correspondencia.strip()
+            inscricoes.append(inscricao)
+
+        # Exibir o texto extraído da página.
         print(text)
+
+# Exibir os endereços extraídos.
+for i, inscricao in enumerate(inscricoes, start=1):
+    print(f"Inscricao {i}: {inscricao}")
