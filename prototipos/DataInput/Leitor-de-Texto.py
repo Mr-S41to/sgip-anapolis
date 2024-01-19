70e2decebf277685a33e1d11b796af09858e94ad
WarWolfOne <matheussaito95k@gmail.com>
2024-01-19 11:54:47 -0300

    Adaptação de padroes de leitura do arquivo para mineração de dados

--- prototipos/DataInput/Leitor-de-Texto.py
+++ prototipos/DataInput/Leitor-de-Texto.py
@@ -1,5 +1,6 @@
 from PyPDF2 import PdfReader
 import re
+import tabula
 
 # Abrir o arquivo PDF em binário.
 with open("sample.pdf", 'rb') as file_pdf:
@@ -11,10 +12,12 @@ with open("sample.pdf", 'rb') as file_pdf:
     # Define um padrão para extrair as informações de endereço.
     padrao_origem = re.compile(r'Inscrição:\s*(.+).*Origem:')
     padrao_inscricao = re.compile(r'Matrícula:\s*(.+).*Inscrição:')
+    padrao_matricula = re.compile(r'Endereço:.*?(\d{3}\.\d{3}\.\d{4}\.\d{3})\s*Matrícula:')
+    #Exemplo de padão de numero de Matrícula: 404.004.0116.000 
     # padrao_origem = re.compile(r'Origem:\s*([\d.]+).*?Inscrição:')
 
     # Os endereços extraídos serão armazenados nesta lista.
-    informacoes = []
+    imoveis = []
 
     # Iterar pelas páginas do PDF.
     for num_pagina in range(num_paginas):
@@ -27,14 +30,25 @@ with open("sample.pdf", 'rb') as file_pdf:
         # Encontrar todas as correspondências para o padrão de endereço em cada página.
         correspondencia_origem = padrao_origem.findall(text, re.DOTALL)
         correspondencia_inscricao = padrao_inscricao.findall(text, re.DOTALL)
+        correspondencia_matricula = padrao_matricula.findall(text, re.DOTALL)
 
         # Processar cada correspondência.
-        for origem, inscricao in zip(correspondencia_origem, correspondencia_inscricao):
+        for origem, inscricao, matricula in zip(correspondencia_origem, correspondencia_inscricao, correspondencia_matricula):
+            #Operador zip para combinar os valores das duas expressões regulares em uma única tupla. 
             origem = origem.strip()
             inscricao = inscricao.strip()
-            informacoes.append((origem, inscricao))
+            matricula = matricula.strip()
+            imoveis.append((origem, inscricao, matricula))
 
+            # # Utilizar tabula para extrair tabelas da página
+            # tabelas = tabula.read_pdf("sample.pdf", pages=num_pagina + 1, multiple_tables=True)
+
+            # # Processar cada tabela encontrada
+            # for tabela in tabelas:
+            #     # Aqui você pode processar a tabela conforme necessário
+            #     print("Tabela encontrada:")
+            #     print(tabela)
 
 # Exibir os endereços extraídos.
-for i, (origem, inscricao) in enumerate(informacoes,  start=1):
-    print(f"{i} Origem: {origem} Inscrição: {inscricao}")
+for i, (origem, inscricao, matricula) in enumerate(imoveis,  start=1):
+    print(f"{i} Origem: {origem} Inscrição: {inscricao} Matrícula: {matricula}")
