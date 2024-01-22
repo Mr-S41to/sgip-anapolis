3c20a04d8b5a83a21ddbae32f6eb66086b3f6cec
WarWolfOne <matheussaito95k@gmail.com>
2024-01-22 15:10:23 -0300

    Extrator de dados em texto funcional

--- prototipos/DataInput/Leitor-de-Texto.py
+++ prototipos/DataInput/Leitor-de-Texto.py
@@ -6,15 +6,16 @@ import tabula
 with open("sample.pdf", 'rb') as file_pdf:
     reader_pdf = PdfReader(file_pdf)
 
-    # Obtendo o número de páginas do PDF.
+    #Obtendo o número de páginas do PDF.
     num_paginas = len(reader_pdf.pages)
 
-    # Define um padrão para extrair as informações de endereço.
-    padrao_origem = re.compile(r'Inscrição:\s*(.+).*Origem:')
-    padrao_inscricao = re.compile(r'Matrícula:\s*(.+).*Inscrição:')
+    #Define um padrão para extrair as informações de endereço.
+    padrao_origem = re.compile(r'Inscrição:\s*(.+?)\s*Origem:')
+    padrao_inscricao = re.compile(r'Matrícula:\s*(.+?)\s*Inscrição:')
     padrao_matricula = re.compile(r'Endereço:.*?(\d{3}\.\d{3}\.\d{4}\.\d{3})\s*Matrícula:')
-    #Exemplo de padão de numero de Matrícula: 404.004.0116.000 
-    padrao_endereco = re.compile(r'Endereço: \s*(.+)(?=\s*Matrícula:)')
+    padrao_endereco = re.compile(r'Endereço:\s*(.+?)\s*(?=\d{3}\.\d{3}\.\d{4}\.\d{3}\s*Matrícula:)')
+    #Ajustado para excluir o nummero de matrícula.
+    padrao_data = re.compile(r'Origem:(.*?)Endereço:', re.DOTALL)
 
     # Os endereços extraídos serão armazenados nesta lista.
     imoveis = []
@@ -23,39 +24,49 @@ with open("sample.pdf", 'rb') as file_pdf:
     for num_pagina in range(num_paginas):
         pagina = reader_pdf.pages[num_pagina]
 
-        # Extração do texto da página.
+        #Extração do texto da página.
         text = pagina.extract_text()
-        print(text)
+
+        # Impressão de texto para depuração
+        print(f"Texto da página {num_pagina + 1}:\n{text}\n")
 
         # Encontrar todas as correspondências para o padrão de endereço em cada página.
-        correspondencia_origem = padrao_origem.findall(text, re.DOTALL)
-        correspondencia_inscricao = padrao_inscricao.findall(text, re.DOTALL)
-        correspondencia_matricula = padrao_matricula.findall(text, re.DOTALL)
-        correspondência_endereco = padrao_endereco.findall(text, re.DOTALL)
+        correspondencia_origem = padrao_origem.findall(text)
+        correspondencia_inscricao = padrao_inscricao.findall(text)
+        correspondencia_matricula = padrao_matricula.findall(text)
+        correspondencia_endereco = padrao_endereco.findall(text)
 
         # Processar cada correspondência.
         for origem, inscricao, matricula, endereco in zip(
             correspondencia_origem, 
             correspondencia_inscricao, 
             correspondencia_matricula, 
-            correspondência_endereco
+            correspondencia_endereco
         ):
-            #Operador zip para combinar os valores das duas expressões regulares em uma única tupla. 
             origem = origem.strip()
             inscricao = inscricao.strip()
             matricula = matricula.strip()
-            endereco = endereco[:-16].strip()
-            imoveis.append((origem, inscricao, matricula, endereco))
+            endereco = endereco.strip()
 
-            # # Utilizar tabula para extrair tabelas da página
-            # tabelas = tabula.read_pdf("sample.pdf", pages=num_pagina + 1, multiple_tables=True)
+            #Aplica a expressão regular para data
+            correspondencia_data = padrao_data.search(text)
 
-            # # Processar cada tabela encontrada
-            # for tabela in tabelas:
-            #     # Aqui você pode processar a tabela conforme necessário
-            #     print("Tabela encontrada:")
-            #     print(tabela)
+            #Verifica se a correspondência foi encontrada.
+            if correspondencia_data:
+                #Obtém a correspondência do grupo de captura.
+                data = correspondencia_data.group(1).strip()
+            else:
+                data = "Padrão não encontrado para esta entrada."
 
-# Exibir os endereços extraídos.
-for i, (origem, inscricao, matricula, endereco) in enumerate(imoveis,  start=1):
-    print(f"{i} \nOrigem: {origem} Inscrição: {inscricao} Matrícula: {matricula} \nEndereço: {endereco}")
+            imoveis.append((origem, inscricao, matricula, endereco, data))
+            
+#Salvar arquivo.
+data_miner = "Data Miner.txt"
+with open(data_miner, 'w') as miner:
+    for (origem, inscricao, matricula, endereco, data) in imoveis:
+        linha = f"{origem} {inscricao} {matricula} {endereco} {data}\n"
+        miner.write(linha)
+        
+# Exibir os resultados.
+for i, (origem, inscricao, matricula, endereco, data) in enumerate(imoveis,  start=1):
+    print(f"{i} \nOrigem: {origem} Inscrição: {inscricao} Matrícula: {matricula} \nEndereço: {endereco} Data: {data}\n")
