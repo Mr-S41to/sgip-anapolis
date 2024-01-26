a83709fb1e5897dd5889b93204e4c4d232570157
WarWolfOne <matheussaito95k@gmail.com>
2024-01-26 11:06:16 -0300

    extração de dados em testes

--- prototipos/DataInput/Leitor-de-Texto.py
+++ prototipos/DataInput/Leitor-de-Texto.py
@@ -2,11 +2,10 @@ from PyPDF2 import PdfReader
 import re
 import pandas as pd
 
-# Open the PDF file in binary mode.
+# Abrir pedef em Binários.
 with open("sample.pdf", 'rb') as file_pdf:
     reader_pdf = PdfReader(file_pdf)
 
-    # Get the number of pages in the PDF.
     num_paginas = len(reader_pdf.pages)
 
     # Define a pattern to extract address information.
@@ -14,12 +13,16 @@ with open("sample.pdf", 'rb') as file_pdf:
     padrao_inscricao = re.compile(r'Matrícula:\s*(.+?)\s*Inscrição:')
     padrao_matricula = re.compile(r'Endereço:.*?(\d{3}\.\d{3}\.\d{4}\.\d{3})\s*Matrícula:' or r'Endereço:\s*(.+?)\s*Matrícula:')
     padrao_endereco = re.compile(r'Endereço:\s*(.+?)\s*(?=\d{3}\.\d{3}\.\d{4}\.\d{3}\s*Matrícula:)')
-    # Adjusted to exclude the matricula number.
+    # Ajustado para excluir o numero de Matrículas.
     padrao_data = re.compile(r'Origem:(.*?)Endereço:', re.DOTALL)
-    padrao_dividas = re.compile(r'Situação:\s*(.+?)\sANO' or r'Situação:\s*(.+?)\sTOTAL ORIGEM:', re.DOTALL)
+    padrao_dividas = re.compile(r'Situação:\s*(.+?)\n\n', re.DOTALL)
     padrao_situacao = re.compile(r'\n*(.+?)\s*Situação:')
-    padrao_linha = re.compile(r'(\d{4}) (\d{2}) ([A-Z]+ - [A-Z]+) (\d+,\d+) (\d+,\d+) (\d+,\d+) (\d+,\d+) (\d+) (\d+)')
-
+    # padrao_linha = re.compile(r'(\d{4}) (\d{2}) (.*?) (\d+(?:,\d+)?) (\d+(?:,\d+)?) (\d+(?:,\d+)?) (\d+(?:,\d+)?) (\d+) (\d+)')
+    # padrao_linha = re.compile(
+    #     r'(\d{4}) (\d{2}) (IPTU|TSU|CIP|IPTU - DA|TSU - DA|CIP - DA|Parcelamento DA|Saldo Parc\. DCC|Saldo Parc\. D\.A|Capin\. RoçaD\.A|Capinação e Roç|Parcelam/Div/Aj) (\d{1,3}(?:\.\d{3})*(?:,\d+)?)|([\d.,]+) (\d{1,3}(?:\.\d{3})*(?:,\d+)?)|([\d.,]+) (\d{1,3}(?:\.\d{3})*(?:,\d+)?)|([\d.,]+) (\d{1,3}(?:\.\d{3})*(?:,\d+)?)|([\d.,]+) (\d+) (\d+)'
+    # )
+    padrao_linha = re.compile(r'(\d{4}) (\d{2}) (.*?) (\d{1,3}(?:\.\d{3})*(?:,\d{2})?|\d+(?:,\d{2})?) (\d{1,3}(?:\.\d{3})*(?:,\d{2})?|\d+(?:,\d{2})?) (\d{1,3}(?:\.\d{3})*(?:,\d{2})?|\d+(?:,\d{2})?) (\d{1,3}(?:\.\d{3})*(?:,\d{2})?|\d+(?:,\d{2})?) (\d+) (\d+)')
+    
     # The extracted objects will be stored in this list.
     imoveis = []
 
@@ -30,10 +33,10 @@ with open("sample.pdf", 'rb') as file_pdf:
         # Extract text from the page.
         text = pagina.extract_text()
 
-        # Print text for debugging.
+        # Debugging de leitura de arquivo.
         print(f"Texto da página {num_pagina + 1}:\n{text}\n")
 
-        # Find all matches for the address pattern on each page.
+        # Correspondencia de Headers.
         correspondencia_origem = padrao_origem.findall(text)
         correspondencia_inscricao = padrao_inscricao.findall(text)
         correspondencia_matricula = padrao_matricula.findall(text)
@@ -48,56 +51,87 @@ with open("sample.pdf", 'rb') as file_pdf:
         ):
             origem = origem.strip()
             inscricao = inscricao.strip()
-            matricula = matricula.strip()
+            if correspondencia_matricula:   
+                matricula = matricula.strip()
+            else:
+                matricula = None
             endereco = endereco.strip()
 
-            # Apply the regular expression for date.
             correspondencia_data = padrao_data.search(text)
-            # Verify if the match was found.
+            # Verificação de correspondencia de dados.
             if correspondencia_data:
-                # Get the match from the capture group.
+                # Obterndo correspondencia de dados.
                 data = correspondencia_data.group(1).strip()
             else:
-                data = "Padrão não encontrado para esta entrada."
+                data = "Padrão de dados em 'data' não encontrado."
+            # Formatando os dados.
             data = re.sub(r'\.(\s*\.)+', '', data)
             data = re.sub(r'ANO MÊS TRIBUTO VL. ATUAL. JUROS MULTA TOTAL VENCIDAS / A VENCER', '', data)
             data = re.sub(r'^.*TOTAL:$', '', data, flags=re.MULTILINE)
             data = re.sub(r'^TOTAL ORIGEM:.*$', '', data, flags=re.MULTILINE)
-
+            
+            situacao = None
             dividas = []
-            if "Situação:" in data:
-                correspondencia_situacao = padrao_situacao.findall(data)
-            for situacao in correspondencia_situacao:
-                divida = []
-                situacao = situacao.strip()
-                for linha in data.strip().split('\n'):
-                    correspondencia_linha = padrao_linha.match(linha)
-                    if correspondencia_linha:
-                        ano, mes, tributo, valor_atual, juros, multa, total, vencidas, a_vencer = correspondencia_linha.groups()
-                        divida.append([ano, mes, tributo, float(valor_atual.replace(',', '.')), float(juros.replace(',', '.')), float(multa.replace(',', '.')), float(total.replace(',', '.')), int(vencidas), int(a_vencer)])
-                # divida = [situacao]
-                divida.append(situacao)
-            dividas.append(divida)
+            divida = []
+            detalhes_divida = []
+            # correspondencia_situacao = padrao_situacao.findall(data)
+            # if correspondencia_situacao:
+            #     situacao = correspondencia_situacao[0].strip()
+            
+            #     for situacao in correspondencia_situacao:
+            #         correspondencia_dividas = padrao_dividas.findall(data)
+            #         if correspondencia_dividas:
+                        
+            #             for dividas_cliente in correspondencia_dividas:
+            #                 for linha in dividas_cliente.strip().split('\n'):
+            #                     correspondencia_linha = padrao_linha.match(linha)
+            #                     if correspondencia_linha:
+            #                         ano, mes, tributo, valor_atual, juros, multa, total, vencidas, a_vencer = correspondencia_linha.groups()
+            #                         divida.append([ano, mes, tributo, valor_atual, juros, multa, total, vencidas, a_vencer])
+            #                     else:
+            #                         divida.append([ano or None, mes or None, tributo or None, valor_atual or None, juros or None, multa or None, total or None, vencidas or None, a_vencer or None])
+            #             dividas.append((situacao, divida))  
+            correspondencia_situacao = padrao_situacao.findall(data)
+            correspondencia_dividas = padrao_dividas.findall(data)
+            if correspondencia_dividas:
+                situacao = correspondencia_situacao[0].strip()
+                for dividas_cliente in correspondencia_dividas:
+                    for linha in dividas_cliente.strip().split('\n'):
+                        correspondencia_linha = padrao_linha.match(linha)
+                        if correspondencia_linha:
+                             ano, mes, tributo, valor_atual, juros, multa, total, vencidas, a_vencer = correspondencia_linha.groups()
+                             detalhes_divida.append([ano, mes, tributo, valor_atual, juros, multa, total, vencidas, a_vencer])
+                        # else:
+                        #     detalhes_divida.append([ano or None, mes or None, tributo or None, valor_atual or None, juros or None, multa or None, total or None, vencidas or None, a_vencer or None])
+                divida.append((situacao, detalhes_divida))
+            dividas.append((divida))   
+            # for dividas_cliente in correspondencia_dividas:
+            # divida = []  # Cria uma nova instância de divida para cada dividas_cliente
+            #     for linha in dividas_cliente.strip().split('\n'):
+            #         correspondencia_linha = padrao_linha.match(linha)
+            #         if correspondencia_linha:
+            #             # ... (código omitido)
+            #             divida.append([ano, mes, tributo, valor_atual, juros, multa, total, vencidas, a_vencer])
+            #         else:
+            #             divida.append([ano or None, mes or None, tributo or None, valor_atual or None, juros or None, multa or None, total or None, vencidas or None, a_vencer or None])
+            #     dividas.append((situacao, divida))  # Adiciona a instância de divida à lista de dividas
+            imoveis.append((origem, inscricao, matricula, endereco, dividas))
 
-            imoveis.append((origem, inscricao, matricula, endereco, data, dividas))
-
-# Display the results.
-for i, (origem, inscricao, matricula, endereco, data, dividas) in enumerate(imoveis, start=1):
-    print(f"{i} \nOrigem: {origem} Inscrição: {inscricao} Matrícula: {matricula} \nEndereço: {endereco} \n\nDados Brutos: {data} \nDividas: {dividas}")
-    for j, divida in enumerate(dividas, start=1):
-        print(f"Divida {j}: {divida}")
+# Depuração de de resultados.
+for i, (origem, inscricao, matricula, endereco, dividas) in enumerate(imoveis, start=1):
+    print(f"{i}\nOrigem: {origem} Inscrição: {inscricao} Matrícula: {matricula} \nEndereço: {endereco} \n{dividas}\n")
+    # for j, divida in enumerate(dividas, start=1):
+    #     print(f"Divida {j}: ")
         
-# Create a DataFrame
-df = pd.DataFrame(imoveis, columns=["Origem:", "Inscrição:", "Matrícula:", "Endereço:", "Dados Brutos", "Dividas:"])
-
-# Display the DataFrame
+# Criar DataFrame
+df = pd.DataFrame(imoveis, columns=["Origem:", "Inscrição:", "Matrícula:", "Endereço:", "Dividas:"])
 print("\n", df)
 
-# Save the DataFrame as CSV
+#Salvando arquivo .CSV.
 df.to_csv("Relatório.csv", index=False)
+print("Arquivo CSV Salvo com sucesso!")
 
-# Save the DataFrame as Excel
+#Salvando arquivo em formato Exel.
 Excel = "Relatório.xlsx"
 df.to_excel(Excel, index=False)
-
-print("Salvo com sucesso!")
+print("Exel Salvo com sucesso!")
