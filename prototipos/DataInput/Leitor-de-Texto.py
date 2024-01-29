1a36c236ec2ef92f7f7888befd340330a7195154
WarWolfOne <matheussaito95k@gmail.com>
2024-01-29 09:23:43 -0300

    Soma de valores

--- prototipos/DataInput/Leitor-de-Texto.py
+++ prototipos/DataInput/Leitor-de-Texto.py
@@ -7,7 +7,6 @@ with open("sample.pdf", 'rb') as file_pdf:
     reader_pdf = PdfReader(file_pdf)
     num_paginas = len(reader_pdf.pages)
 
-    # Define a pattern to extract address information.
     # padrao_origem = re.compile(r'Inscrição:\s*(.+?)\s*Origem:')
     # padrao_inscricao = re.compile(r'Matrícula:\s*(.+?)\s*Inscrição:')
     # padrao_matricula = re.compile(r'Endereço:.*?(\d{3}\.\d{3}\.\d{4}\.\d{3})\s*Matrícula:')
@@ -19,18 +18,19 @@ with open("sample.pdf", 'rb') as file_pdf:
     # padrao_linha = re.compile(r'(\d{4}) (\d{2}) (.*?) (\d{1,3}(?:\.\d{3})*(?:,\d{2})?|\d+(?:,\d{2})?) (\d{1,3}(?:\.\d{3})*(?:,\d{2})?|\d+(?:,\d{2})?) (\d{1,3}(?:\.\d{3})*(?:,\d{2})?|\d+(?:,\d{2})?) (\d{1,3}(?:\.\d{3})*(?:,\d{2})?|\d+(?:,\d{2})?) (\d+) (\d+)')
 
     imoveis = []
+    total_solicitante = 0
     
     # Iterate through the pages of the PDF.
     for num_pagina in range(num_paginas):
         pagina = reader_pdf.pages[num_pagina]
-        # Extract text from the page.
+        # Extração do texto das paginas.
         text = pagina.extract_text()
         # Debugging de leitura de arquivo.
         print(f"Texto da página {num_pagina + 1}:\n{text}\n")
         
         padrao_origem = re.compile(r'Inscrição:\s*(.+?)\s*Origem:')
         padrao_inscricao = re.compile(r'Matrícula:\s*(.+?)\s*Inscrição:')
-        padrao_matricula = re.compile(r'Endereço:.*?(\d{3}\.\d{3}\.\d{4}\.\d{3})\s*Matrícula:')
+        padrao_matricula = re.compile(r'Endereço:.*?(\d{3}\.\d{3}\.\d{4}\.\d{3})\s*Matrícula:' or r'Endereco:\..*?(\d{4})')
         padrao_endereco = re.compile(r'Endereço:\s*(.+?)\s*(?=\d{3}\.\d{3}\.\d{4}\.\d{3}\s*Matrícula:)')
 
         # Correspondencia de Headers.
@@ -48,13 +48,13 @@ with open("sample.pdf", 'rb') as file_pdf:
         ):
             origem = origem.strip()
             inscricao = inscricao.strip()
-            matricula = matricula.strip() if correspondencia_matricula else None
+            matricula = matricula.strip()
+            matricula = matricula.strip() if matricula else False
             endereco = endereco.strip()
             
-            padrao_data = re.compile(r'Origem:(.*?)Endereço:', re.DOTALL)
+            padrao_data = re.compile(r'Origem:(.*?)Endereço:', re.DOTALL) or re.compile(r'Origem:(.*?)OBSERVAÇÃO:', re.DOTALL)
             data = padrao_data.search(text).group(1).strip()
             # correspondencia_data = padrao_data.search(text)
-            # # Verificação de correspondencia de dados.
             # if correspondencia_data:
             #     # Obterndo correspondencia de dados.
             #     data = correspondencia_data.group(1).strip()
@@ -73,10 +73,12 @@ with open("sample.pdf", 'rb') as file_pdf:
             
             situacao = correspondencia_situacao[0].strip() if correspondencia_situacao else None
             dividas = []
+            total_origem = 0
             
             if correspondencia_dividas and correspondencia_situacao:
                 for dividas_cliente in correspondencia_dividas:
                     divida = []
+                    total_divida = 0       
                     for linha in dividas_cliente.strip().split('\n'):
                         padrao_linha = re.compile(r'(\d{4}) (\d{2}) (.*?) (\d{1,3}(?:\.\d{3})*(?:,\d{2})?|\d+(?:,\d{2})?) (\d{1,3}(?:\.\d{3})*(?:,\d{2})?|\d+(?:,\d{2})?) (\d{1,3}(?:\.\d{3})*(?:,\d{2})?|\d+(?:,\d{2})?) (\d{1,3}(?:\.\d{3})*(?:,\d{2})?|\d+(?:,\d{2})?) (\d+) (\d+)')
                         correspondencia_linha = padrao_linha.match(linha)
@@ -88,8 +90,10 @@ with open("sample.pdf", 'rb') as file_pdf:
                             multa = multa.replace(".", "").replace(",",".")
                             total = total.replace(".", "").replace(",",".")
                             ano, mes, tributo = map(str, [ano, mes, tributo])
-                            valor_atual, juros, multa, total = map(float, [valor_atual, juros, multa, total])
+                            valor_atual, juros, multa = map(float, [valor_atual, juros, multa])
+                            # total = valor_atual + juros + multa = map(float, )
                             vencidas, a_vencer = map(int, [vencidas, a_vencer])
+                            total_divida += float(total)
                             divida.append({
                                 "Ano": ano,
                                 "Mês": mes,
@@ -97,6 +101,7 @@ with open("sample.pdf", 'rb') as file_pdf:
                                 "Valor Atual": valor_atual,
                                 "Juros": juros,
                                 "Multa": multa,
+                                "Total": total,
                                 "Vencidas": vencidas,
                                 "A Vencer": a_vencer
                             })
@@ -108,9 +113,11 @@ with open("sample.pdf", 'rb') as file_pdf:
                             #  detalhes_divida.append([ano, mes, tributo, valor_atual, juros, multa, total, vencidas, a_vencer])
                         # else:
                         #     detalhes_divida.append([ano or None, mes or None, tributo or None, valor_atual or None, juros or None, multa or None, total or None, vencidas or None, a_vencer or None])
+                    total_origem += float(total_divida)
                     dividas.append({
                         "Situação": situacao, 
-                        "Divida": divida
+                        "Divida": divida,
+                        "Total Origem": total_origem
                     })
             
             imoveis.append({
@@ -118,10 +125,12 @@ with open("sample.pdf", 'rb') as file_pdf:
                 "Inscrição": inscricao,
                 "Matrícula": matricula,
                 "Endereço": endereco,
-                "Dívidas": dividas
+                "Dívidas": dividas,
+                # "Data": data
             })
+            
+            total_solicitante += float(total_origem)
 
-# Depuração de de resultados.
 # Depuração de resultados.
 for i, imovel in enumerate(imoveis, start=1):
     origem = imovel['Origem']
@@ -130,21 +139,22 @@ for i, imovel in enumerate(imoveis, start=1):
     endereco = imovel['Endereço']
     dividas = imovel['Dívidas']
 
-    print(f"{i}\nOrigem: {origem} Inscrição: {inscricao} Matrícula: {matricula} \nEndereço: {endereco} \n{dividas}\n")
-
-# for i, imovel in enumerate(imoveis, start=1):
-    # print(f"{i}\nOrigem: {imovel['Origem']} Inscrição: {inscricao} Matrícula: {matricula} \nEndereço: {endereco} \n{dividas}\n")
-    # for j, divida in enumerate(dividas, start=1):
-    #     print(f"Divida {j}: ")
-        
+    print(f"{i}\nOrigem: {origem} Inscrição: {inscricao} Matricula: {matricula}  \nEndereço: {endereco} \nDividas: {dividas}\n")
+    print(f"Total Solicitante: {total_solicitante}")
 # Criar DataFrame
-df = pd.DataFrame(imoveis, columns=["Origem:", "Inscrição:", "Matrícula:", "Endereço:", "Dividas:"])
+df = pd.DataFrame(imoveis, columns=["Origem", "Inscrição",  "Endereço", "Dívidas"])
+
+# Transformar a coluna "Dívidas" em string para evitar problemas com listas
+# df["Dívidas"] = df["Dívidas"].astype(str)
+
+# Depuração do DataFrame
 print("\n", df)
 
-#Salvando arquivo .CSV.
+# Salvando arquivo .CSV
 df.to_csv("Relatório.csv", index=False)
 print("Arquivo CSV Salvo com sucesso!")
-#Salvando arquivo em formato Exel.
+
+# Salvando arquivo em formato Excel
 Excel = "Relatório.xlsx"
 df.to_excel(Excel, index=False)
-print("Exel Salvo com sucesso!")
+print("Excel Salvo com sucesso!")
--- prototipos/DataInput/Separador-de-Milha.py
+++
@@ -1,7 +0,0 @@
-from decimal import Decimal
-
-a = Decimal("20,3".replace(",", "."))
-b = Decimal("19,7".replace(",", "."))
-c = a + b
-
-print(c)
